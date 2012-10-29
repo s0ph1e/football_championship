6ecb97e6c9fee2ec14dfaec4ff04a5caf4c0ff35
s0ph1e <sonya1912@gmail.com>
2012-10-29 21:42:41 +0200

    Добавлены кнопки изменения, удаления, добавления команды

--- application/views/team_list.php
+++ application/views/team_list.php
@@ -5,16 +5,18 @@
                         'heading_row_start'   => '<tr class = "teams_table_heading">',
                          'row_start'           => '<tr class = "teams_table_row">');
         $this->table->set_template($tmpl);
-        $this->table->set_caption('Список команд');
-        $this->table->set_heading(array('id', 'Команда', 'Город', 'Тренер'));
+        $this->table->set_heading(array('id', 'Команда', 'Город', 'Тренер', 'Действия'));
         
         // Добавляем в таблицу все команды
         foreach ($teams as $team)
         {
-            $this->table->add_row($team->id, $team->team, $team->city, $team->trainer);
+            $actions = '<center>'.anchor(site_url('team/update/'.$team->id), img('data/images/edit.png'), array('title'=>"Изменить"));
+            $actions .='&nbsp'.anchor(site_url('team/delete/'.$team->id), img('data/images/delete.png'), array('title'=>"Удалить"));
+            $this->table->add_row($team->id, $team->team, $team->city, $team->trainer, $actions);
         }
         // Вывод таблицы команд
         echo $this->table->generate();
         $this->table->clear();
     }
-    else echo 'Нет команд.';
+    else {echo 'Нет команд.';}
+    echo 'Вы можете добавить команду '.anchor(site_url('team/add'), img('data/images/add.png'), array('title'=>"Добавить"));
