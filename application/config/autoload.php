b6491d3f2d845cbf57b567eb40cdfa8e59a28c5c
s0ph1e <sonya1912@gmail.com>
2012-10-29 21:21:19 +0200

    Добавлена таблица вывода команд

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'table');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'html', 'table');
+$autoload['helper'] = array('url', 'html');
 
 
 /*
--- application/views/team_list.php
+++ application/views/team_list.php
@@ -1,5 +1,20 @@
 <?php
-    foreach ($teams as $team)
+    if ($teams)
     {
-        echo $team->team;
+        $tmpl = array ( 'table_open'  => '<table class = "teams_table">',
+                        'heading_row_start'   => '<tr class = "teams_table_heading">',
+                         'row_start'           => '<tr class = "teams_table_row">');
+        $this->table->set_template($tmpl);
+        $this->table->set_caption('Список команд');
+        $this->table->set_heading(array('id', 'Команда', 'Город', 'Тренер'));
+        
+        // Добавляем в таблицу все команды
+        foreach ($teams as $team)
+        {
+            $this->table->add_row($team->id, $team->team, $team->city, $team->trainer);
+        }
+        // Вывод таблицы команд
+        echo $this->table->generate();
+        $this->table->clear();
     }
+    else echo 'Нет команд.';
