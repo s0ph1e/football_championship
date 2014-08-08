f803c7be834bf79a9f91d3a327a70449d8a6e271
s0ph1e <sonya1912@gmail.com>
2014-08-08 12:24:06 +0300

    add db and config file

---
+++ application/config/table_template.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['table_template'] = array ( 'table_open'  => '<table class = "teams_table">',
+                        'heading_row_start'   => '<tr class = "teams_table_heading">',
+                        'row_start'           => '<tr class = "teams_table_row">',
+                        'row_alt_start'   =>  '<tr class = "teams_table_row">'
+               );
+
+/* End of file templates.php */
+/* Location: ./application/config/templates.php */
